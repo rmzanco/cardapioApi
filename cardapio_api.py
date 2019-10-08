7146ac5f34e4c0608c0f444828f7d45939620946
rodri <rmzancs@gmail.com>
2019-10-08 13:31:59 -0300

    Commit Inicial - Já rodando sem interface Proximo passo: Implementar dias diferentes c/ beautifulSoup

---
+++ cardapio_api.py
@@ -0,0 +1,44 @@
+# Rodrigo Malosti Zanco - Sistemas de Informação 017
+# Sistema que através de request pode ser resgatado o cardápio do Restaurante Universitário de Limeira (FT/FCA)
+
+import requests
+import html
+import re
+
+URL = "http://www.sar.unicamp.br/RU/view/site/cardapio.php"
+
+try:
+    resp = requests.get(URL)
+    txt_conteudo = resp.text
+    # parser p/ converter html chars
+    txt_parser = html.unescape(txt_conteudo)
+    # transformar cada linha da str em uma lista
+    lista_conteudo = txt_parser.splitlines()
+
+    i = 0
+    for l in lista_conteudo:
+
+        result = re.search("ARROZ", l)
+        if result is not None:
+            arroz = l[52:-5]
+            prato = lista_conteudo[i + 3][89:-5]
+            guarnicao = lista_conteudo[i + 6][80:-5]
+            proteina = lista_conteudo[i + 9][79:-5]
+            salada = lista_conteudo[i + 12][77:-5]
+            sobremesa = lista_conteudo[i + 15][80:-5]
+            suco = lista_conteudo[i + 18][75:-5]
+
+            print("Cardápio de hoje:\n",
+                  arroz,     ",",
+                  prato,     ",",
+                  guarnicao, ",",
+                  proteina,  ",",
+                  salada,    ",",
+                  sobremesa, ",",
+                  "Suco de", suco)
+
+        i = i + 1
+
+
+except:
+    print("Site Indisponível/Fora de operação")
