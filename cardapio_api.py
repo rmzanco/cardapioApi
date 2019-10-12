1abac4897faeefb04b9db07b3ba6dc57eafe3fe8
Rodrigo Zanco <rmzancs@gmail.com>
2019-10-12 11:33:17 -0300

    Formatação da saída do arquivo cardapio_api.py Proximo passo: -Utilizar beautiful soup para a coleta de informações; -Criar um arquivo .csv para inserção dos dados desejados.

--- cardapio_api.py
+++ cardapio_api.py
@@ -16,11 +16,27 @@ try:
     # transformar cada linha da str em uma lista
     lista_conteudo = txt_parser.splitlines()
 
-    i = 0
-    for l in lista_conteudo:
+    i = 0  # contador da lista
+    tipo = 0  # critério utilizado para definir o tipo de cardápio
+    horario = 0  # criterio utilizado para separar as definições de refeição almoço/janta
 
+    for l in lista_conteudo:
         result = re.search("ARROZ", l)
         if result is not None:
+
+            tipo = tipo + 1
+            
+            if tipo % 2 == 0:
+                cardapio = "vegetariano"
+            else:
+                cardapio = "convencional"
+
+            if horario <= 1:
+                tipo_horario = "do almoço"
+                horario = horario + 1
+            else:
+                tipo_horario = "da janta"
+
             arroz = l[52:-5]
             prato = lista_conteudo[i + 3][89:-5]
             guarnicao = lista_conteudo[i + 6][80:-5]
@@ -29,7 +45,7 @@ try:
             sobremesa = lista_conteudo[i + 15][80:-5]
             suco = lista_conteudo[i + 18][75:-5]
 
-            print("Cardápio de hoje:\n",
+            print("Cardápio", cardapio, tipo_horario, "de hoje:\n",
                   arroz,     ",",
                   prato,     ",",
                   guarnicao, ",",
@@ -37,7 +53,6 @@ try:
                   salada,    ",",
                   sobremesa, ",",
                   "Suco de", suco)
-
         i = i + 1
 
 
